1479b29c9fad2dc0a9c07da72992465cd50665ed
arrvind <141898022+arrvindops@users.noreply.github.com>
2024-04-27 18:52:30 +0530

    listing source files

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -18,6 +18,7 @@ stages {
     
     stage ('Code Quality'){
         steps {
+            sh 'ls -l'
             sh 'sonar-scanner -Dsonar.projectKey=${component} -Dsonar.host.url=http://172.31.29.145:9000 -Dsonar.login=admin -Dsonar.password=admin123'
         }
     }
