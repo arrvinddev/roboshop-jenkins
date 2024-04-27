7b7629b681707136d6223601da00acd38dacccc1
arrvind <141898022+arrvindops@users.noreply.github.com>
2024-04-27 18:28:39 +0530

    sonar creds

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -18,7 +18,7 @@ stages {
     
     stage ('Code Quality'){
         steps {
-            sh 'sonar-scanner -Dsonar.projectKey=${component} -Dsonar.host.url=http://172.31.29.145:9000'
+            sh 'sonar-scanner -Dsonar.projectKey=${component} -Dsonar.host.url=http://172.31.29.145:9000 -Dsonar.login=admin -Dsonar.password=admin123'
         }
     }
 
