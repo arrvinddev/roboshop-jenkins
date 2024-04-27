f96239bea5e606af8f232ed06495d68992f168e3
arrvind <141898022+arrvindops@users.noreply.github.com>
2024-04-27 18:25:21 +0530

    host server url for sonar

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -18,7 +18,7 @@ stages {
     
     stage ('Code Quality'){
         steps {
-            sh 'sonar-scanner -Dsonar.projectKey=${component}'
+            sh 'sonar-scanner -Dsonar.projectKey=${component} -Dsonar.host.url=http://172.31.29.145:9000'
         }
     }
 
