d973ba78ae489b0e6d757c666cfb0b62c1a68d3a
arrvind <141898022+arrvindops@users.noreply.github.com>
2024-04-29 13:09:06 +0530

    unit test cases nodejs

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -16,12 +16,12 @@ pipeline {
 
 stages {
     
-    stage ('Code Quality'){
-        steps {
+    // stage ('Code Quality'){
+    //     steps {
             // sh 'ls -l'
             // sh 'sonar-scanner -Dsonar.projectKey=${component} -Dsonar.host.url=http://172.31.29.145:9000 -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.qualitygate.wait=true'
-        }
-    }
+    //     }
+    // }
 
     stage ('Unit Test Cases'){
         steps {
