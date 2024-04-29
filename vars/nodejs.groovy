154e8a5a28cabbd080d997492e534d7157008667
arrvind <141898022+arrvindops@users.noreply.github.com>
2024-04-29 11:31:02 +0530

    sonarqube quality gate option

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -19,7 +19,7 @@ stages {
     stage ('Code Quality'){
         steps {
             sh 'ls -l'
-            sh 'sonar-scanner -Dsonar.projectKey=${component} -Dsonar.host.url=http://172.31.29.145:9000 -Dsonar.login=admin -Dsonar.password=admin123'
+            sh 'sonar-scanner -Dsonar.projectKey=${component} -Dsonar.host.url=http://172.31.29.145:9000 -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.qualitygate.wait=true'
         }
     }
 
