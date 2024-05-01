83a1c6a29f05979c64b4bf67c3590df5a6155469
arrvind <141898022+arrvindops@users.noreply.github.com>
2024-05-01 10:37:19 +0530

    release stage application

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -19,33 +19,35 @@ stages {
 
     stage ('Code Compile'){
         steps {
-            sh "mvn compile"
+            // sh "mvn compile"
+            sh 'echo compile'
         }
     }
     
     stage ('Code Quality'){
         steps {
-            sh 'ls -l'
+            // sh 'ls -l'
             // sh 'sonar-scanner -Dsonar.projectKey=${component} -Dsonar.host.url=http://172.31.29.145:9000 -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.qualitygate.wait=true -Dsonar.java.binaries=./target'
+            sh 'echo Code Quality'
         }
     }
 
     stage ('Unit Test Cases'){
         steps {
-            sh "echo Unit tests"
+            sh 'echo Unit tests'
             // sh "mvn test"
         }
     }
 
     stage ('CheckMarx SAST Scan'){
         steps {
-            sh "echo CheckMarx Scan"
+            sh 'echo CheckMarx Scan'
         }
     }
 
     stage ('CheckMarx SCA Scan'){
         steps {
-            sh "echo CheckMarx SCA Scan"
+            sh 'echo CheckMarx SCA Scan'
         }
     }
 
