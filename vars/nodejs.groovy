58ebed2ccf0a12a44622823edfb97dfb29024061
arrvind <141898022+arrvindops@users.noreply.github.com>
2024-05-02 16:46:06 +0530

    schema dir

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -49,7 +49,7 @@ stages {
         steps {
             sh 'npm install'
             sh 'echo $TAG_NAME >VERSION'
-            sh 'zip -r ${component}-${TAG_NAME}.zip node_modules server.js VERSION'
+            sh 'zip -r ${component}-${TAG_NAME}.zip node_modules server.js VERSION ${schema_dir}'
             sh 'curl -f -v -u ${NEXUS_USR}:${NEXUS_PSW} --upload-file ${component}-${TAG_NAME}.zip http://172.31.30.201:8081/repository/${component}/${component}-${TAG_NAME}.zip'
         }
     }
