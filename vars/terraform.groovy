3e0edae5fe6dfacb4a50691b80f51b69d0a3db49
arrvind <141898022+arrvindops@users.noreply.github.com>
2024-05-06 10:39:39 +0530

    terraform init

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -12,45 +12,45 @@ pipeline {
   }
 
 
-// parameters { 
-// choice(name: 'env', choices: ['dev', 'prod'], description: 'choose environment to apply')
-//  }
-
-// stages {
-
-//     stage('Terraform INIT') {
-//         steps {
-//             sh 'terraform init -backend-config=env-${env}/state.tfvars'
-//         }
-//     }
-//     stage('Terraform Apply') {
-//         steps {
-//             sh 'terraform apply -auto-approve -var-file=env-${env}/main.tfvars'
-//         }
-//     }
-
-    
-// }
-
 parameters { 
-choice(name: 'env', choices: ['dev', 'prod'], description: 'choose environment to destroy')
+choice(name: 'env', choices: ['dev', 'prod'], description: 'choose environment to apply')
  }
 
- stages {
+stages {
 
     stage('Terraform INIT') {
         steps {
             sh 'terraform init -backend-config=env-${env}/state.tfvars'
         }
     }
-    stage('Terraform destroy') {
+    stage('Terraform Apply') {
         steps {
-            sh 'terraform destroy -auto-approve -var-file=env-${env}/main.tfvars'
+            sh 'terraform apply -auto-approve -var-file=env-${env}/main.tfvars'
         }
     }
 
     
 }
+
+// parameters { 
+// choice(name: 'env', choices: ['dev', 'prod'], description: 'choose environment to destroy')
+//  }
+
+//  stages {
+
+//     stage('Terraform INIT') {
+//         steps {
+//             sh 'terraform init -backend-config=env-${env}/state.tfvars'
+//         }
+//     }
+//     stage('Terraform destroy') {
+//         steps {
+//             sh 'terraform destroy -auto-approve -var-file=env-${env}/main.tfvars'
+//         }
+//     }
+
+    
+// }
 
 
 
