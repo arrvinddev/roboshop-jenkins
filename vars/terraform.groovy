5e78d7eea93d6761a8346ad63cd08eb5fd4e2374
arrvind <141898022+arrvindops@users.noreply.github.com>
2024-05-08 10:26:03 +0530

    terraform groovy

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -1,66 +1,44 @@
 def call() {
+  pipeline {
 
-pipeline { 
     agent {
-        node {
-            label 'workstation'
-        }
+      node {
+        label 'workstation'
+      }
     }
 
-  options { 
-    ansiColor('xterm')
+    options {
+      ansiColor('xterm')
+    }
+
+    parameters {
+      choice(name: 'env', choices: ['dev', 'prod'], description: 'Pick environment')
+      choice(name: 'action', choices: ['apply', 'destroy'], description: 'Pick Action')
+    }
+
+    stages {
+
+      stage('Terraform INIT') {
+        steps {
+          sh 'terraform init -backend-config=env-${env}/state.tfvars'
+        }
+      }
+
+      stage('Terraform Apply') {
+        steps {
+          sh 'terraform ${action} -auto-approve -var-file=env-${env}/main.tfvars'
+        }
+      }
+
+    }
+
+    post {
+      always {
+        cleanWs()
+      }
+    }
+
   }
 
 
-parameters { 
-choice(name: 'env', choices: ['dev', 'prod'], description: 'Pick environment')
-// choice(name: 'action' choices: ['apply', 'destroy'], description: 'Pick action')
- }
-
-stages {
-
-    stage('Terraform INIT') {
-        steps {
-            sh 'terraform init -backend-config=env-${env}/state.tfvars'
-        }
-    }
-    stage('Terraform Apply') { 
-        steps {
-            //  sh 'terraform plan -var-file=env-${env}/main.tfvars'
-            sh 'terraform destroy -auto-approve -var-file=env-${env}/main.tfvars'
-            // sh 'terraform ${action} -auto-approve -var-file=env-${env}/main.tfvars'
-        }
-    }
-
-    
-}
-
-// parameters { 
-// choice(name: 'env', choices: ['dev', 'prod'], description: 'choose environment to destroy')
-//  }
-
-//  stages {
-
-//     stage('Terraform INIT') {
-//         steps {
-//             sh 'terraform init -backend-config=env-${env}/state.tfvars'
-//         }
-//     }
-//     stage('Terraform destroy') {
-//         steps {
-//             sh 'terraform destroy -auto-approve -var-file=env-${env}/main.tfvars'
-//         }
-//     }
-
-    
-// }
-
-
-
-post { 
-     always {
-        cleanWs()
-     }
-}
-}
 }
